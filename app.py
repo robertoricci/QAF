f644fe73add0b76c23aff3c453519719fd857580
roberto ricci <roberto.rricci@gmail.com>
2022-09-27 12:29:54 -0300

    commit inicial

---
+++ app.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import style as style
+import app_home
+import app_carteira
+import app_bolsa_eleicoes
+import pandas as pd
+
+
+import matplotlib
+matplotlib.use('Agg')
+
+from streamlit_option_menu import option_menu
+
+import warnings
+warnings.filterwarnings('ignore')
+ 
+import datetime as dt 
+dia = dt.datetime.today().strftime(format='20%y-%m-%d')
+
+
+st.set_page_config(  # Alternate names: setup_page, page, layout
+    layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+    initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
+    page_title="QAF",  # String or None. Strings get appended with "• Streamlit". 
+    page_icon= './images/QAF.png',  # String, anything supported by st.image, or None.
+)
+
+
+#carrega os arquivos css
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    
+    
+def main(): 
+    pages={
+         "Home":page_home,
+          "Eleições":page_bolsa_eleicoes
+    }
+    
+        #esconder botão de menu e marca dágua no rodapé
+    style.hidden_menu_and_footer()
+        #cabeçalho detalhe superior da página 
+    style.headerstyle()
+           
+    with st.sidebar:
+            style.sidebarwidth() 
+            page = option_menu('Menu', ["Home","Eleições"],
+                                icons=['house','cash-coin'],
+                                default_index=0, menu_icon="app-indicator",   #orientation='horizontal',
+                                styles={
+                "container": {"padding": "2!important", "background-color": "#ffffff","margin": "0px" }, # ,"background-size": "cover","margin": "0px"},
+                "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"}, #,"position": "relative","display": "inline"},
+                "nav-link-selected": {"background-color": "#4a7198"},
+                }) 
+            
+    pages[page]()
+    
+    with st.sidebar.expander('Sobre'):
+        # Mostrar versões das bibliotecas
+        #st.write(os.popen(f'python --version').read())
+        #st.write('Streamlit:', st.__version__)
+        #st.write('Pandas:', pd.__version__)
+        #st.write('yfinance:', yf.__version__)
+        #st.write('plotly:', plotly.__version__)
+        #st.write('Fundamentus:', fundamentus.__version__)
+        st.write('Feito com Carinho')
+        st.write('Roberto Carlos Ricci')
+        st.write('pbisolucoes@gmail.com')
+        st.write('https://bit.ly/lirobertocarlosricci')
+       
+if 'portifolio' not in st.session_state:
+        st.session_state.portifolio = pd.DataFrame()
+        st.session_state.portifolio['Ação'] = ''
+    
+def page_home():
+     app_home.home()
+
+def page_carteira():
+     app_carteira.carteira()
+
+
+def page_bolsa_eleicoes():
+     st.session_state.tabela_papeis = puxar_tabela_papeis()
+     app_bolsa_eleicoes.bolsa()
+ 
+  
+    
+@st.cache
+def puxar_tabela_papeis():
+    return pd.read_csv('./dados/tickers.csv')
+
+
+  
+if __name__ == "__main__":
+   main()
---
+++ app_bolsa_eleicoes.py
@@ -0,0 +1,169 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import plotly.express as px
+#import numpy as np
+#import pandas as pd
+# import yfinance as yf
+# #import time
+# import matplotlib.pyplot as plt
+# import plotly.express as px
+# import seaborn as sns
+# import cufflinks as cf
+# #import datetime
+# #from datetime import date
+# import math
+# import fundamentus
+
+
+# st.set_page_config(  # Alternate names: setup_page, page, layout
+# 	layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+# 	initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
+# 	page_title="",  # String or None. Strings get appended with "• Streamlit". 
+# 	page_icon=None,  # String, anything supported by st.image, or None.
+# )
+
+def bolsa():
+         #código para ativar bootstrap css
+    st.markdown(
+"""
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+""",unsafe_allow_html=True
+    ) 
+    
+    st.title('Análise Bolsa de Valores nas ultimas eleições presidencias')
+
+    df = puxar_dados('^BVSP','1998-01-01','2022-09-30')
+    
+    df['ano'] = df.index.year
+    df['dia_do_ano'] = df.index.dayofyear
+# nova coluna data e ano
+
+    tb = df.pivot( index='dia_do_ano', columns='ano', values='Adj Close')
+    tab = tb.fillna(method='bfill')
+
+    tab.pct_change()
+
+    tab = ( tab/tab.iloc[0] ) - 1
+
+    #gerar grafico
+    gerar_grafico(tab)
+    
+
+def gerar_grafico_unid(tab,ticker):
+    #anos das eleições
+    listano = tab.columns
+    anosvalido = [1998,2002,2006,2014,2018] 
+    
+    anos = [s for s in listano if s in anosvalido ]
+    
+    tabf = tab[anos]
+    
+    fig = px.line(
+        tabf,
+        height=600,
+        width=1000,
+        labels={'Value':'retorno'}
+    )
+    fig.add_vline( x= pd.to_datetime('2022-10-01').dayofyear ,line_dash="dash",line_color="blue")
+    fig.add_vline( x= pd.to_datetime('2022-10-31').dayofyear, line_dash="dash",line_color="blue")
+    fig.update_traces(line=dict(width=1)
+                      
+    )
+    
+    fig.add_scatter(
+        x=tab.index,
+        y=tab[2022],
+        name='2022',
+        line=dict(width=3),
+    )
+    # fig.update_traces(hovertemplate='dia_ano: %{x} <br>Retorno: %{y}  ')
+    
+    fig.update_traces( hovertemplate='<b>Retorno: </b> %{y:.0%}'+
+                                       '<br><b>Dia Ano:</b> %{x:}')
+    
+    fig.layout.yaxis.tickformat = '.0%'
+    
+    anos = anos + [2022]
+
+    fig.update_layout(title='Performance do Ativo '+ ticker + ' nos anos eleitorais ' + str(anos),
+                   xaxis_title='Dia do Ano',
+                   yaxis_title='Retorno',
+                   template='plotly_dark')
+    st.plotly_chart(fig,use_container_width=True)   
+    
+@st.cache(allow_output_mutation=True)
+def puxar_dados(ticker,dt_ini,dt_fim):
+    precos = yf.download(ticker, start=dt_ini, end=dt_fim, progress=False)[['Adj Close']]
+    #precos = precos.fillna(method='bfill')
+    return precos
+
+def gerar_grafico(tab):
+    #anos das eleições
+    listano = tab.columns
+    anosvalido = [1998,2002,2006,2014,2018] 
+    
+    anos = [s for s in listano if s in anosvalido ]
+    
+    tabf = tab[anos]
+    
+    fig = px.line(
+        tabf,
+        height=600,
+        width=1000,
+        labels={'Value':'retorno'}
+    )
+    fig.add_vline( x= pd.to_datetime('2022-10-01').dayofyear ,line_dash="dash",line_color="blue")
+    fig.add_vline( x= pd.to_datetime('2022-10-31').dayofyear, line_dash="dash",line_color="blue")
+    fig.update_traces(line=dict(width=1)
+                      
+    )
+    
+    fig.add_scatter(
+        x=tab.index,
+        y=tab[2022],
+        name='2022',
+        line=dict(width=3),
+    )
+    # fig.update_traces(hovertemplate='dia_ano: %{x} <br>Retorno: %{y}  ')
+    
+    fig.update_traces( hovertemplate='<b>Retorno: </b> %{y:.0%}'+
+                                       '<br><b>Dia Ano:</b> %{x:}')
+    
+    fig.layout.yaxis.tickformat = '.0%'
+    
+
+    fig.update_layout(title='Performance Indice Bovespa (IBOV) no anos eleitorias [1998,2002,2006,2014,2018 e 2022]',
+                   xaxis_title='Dia do Ano',
+                   yaxis_title='Retorno',
+                   template='plotly_dark')
+
+    st.plotly_chart(fig,use_container_width=True)
+
+    st.header('Análise da perfomance de um único ativo')
+
+    st.write('')
+    
+    col1, col2 = st.columns(2)
+    with col1:
+        ticker = st.selectbox('Escolha o Ativo (Clique no campo e digite as iniciais do Ticker)',st.session_state.tabela_papeis['Ticker'])
+    
+    with col2:
+        st.write('')
+        st.write('')
+        bt_calc = st.button('Listar')
+
+   
+    if bt_calc:
+        df = puxar_dados(ticker + '.SA','1998-01-01','2022-09-30')
+        df['ano'] = df.index.year
+        df['dia_do_ano'] = df.index.dayofyear
+
+        tb = df.pivot( index='dia_do_ano', columns='ano', values='Adj Close')
+        tab = tb.fillna(method='bfill')
+        tab.pct_change()
+        tab = ( tab/tab.iloc[0] ) - 1
+    
+        gerar_grafico_unid(tab,ticker)
+        
